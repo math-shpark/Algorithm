1242205e3eabea09ff3fdbc739937b51f4bbd8db
math-shpark <psuhoo122065@gmail.com>
2022-03-10 10:47:41 +0900

    Updated on Mar 10 2022 10:47

---
+++ Bronze/src/level5/Problem5338.java
@@ -0,0 +1,14 @@
+package level5;
+
+public class Problem5338 {
+
+	// 5338. 마이크로소프트 로고
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+
+}
