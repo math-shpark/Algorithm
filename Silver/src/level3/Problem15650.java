b4b89bf4dab1304a0c43cbf1a34a9e8fd9757eac
Su-ho Park <psuhoo122065@gmail.com>
2022-04-04 11:23:34 +0900

    Updated on Apr 04 2022 11:23

--- Silver/src/level3/Problem15650.java
+++ Silver/src/level3/Problem15650.java
@@ -1,28 +1,31 @@
 package level3;
 
 import java.util.Scanner;
- 
-public class Main {
- 
+
+public class Problem15650 {
+
 	public static int[] arr;
 	public static int N, M;
- 
+
+	// 15650. N과 M(2)
 	public static void main(String[] args) {
- 
+
 		Scanner scan = new Scanner(System.in);
-    // 숫자 범위 입력
+		// 숫자 범위 입력
 		N = scan.nextInt();
-    // 수열 길이 입력
+		// 수열 길이 입력
 		M = scan.nextInt();
-    //  뽑은 수열을 저장할 배열 크기 입력
+		// 뽑은 수열을 저장할 배열 크기 입력
 		arr = new int[M];
-    // dfs
+		// dfs
 		dfs(1, 0);
- 
+
+		scan.close();
+
 	}
- 
+
 	public static void dfs(int at, int depth) {
- 
+
 		if (depth == M) {
 			for (int val : arr) {
 				System.out.print(val + " ");
@@ -30,12 +33,12 @@ public class Main {
 			System.out.println();
 			return;
 		}
-        
+
 		for (int i = at; i <= N; i++) {
- 
+
 			arr[depth] = i;
 			dfs(i + 1, depth + 1);
- 
+
 		}
 	}
 }
