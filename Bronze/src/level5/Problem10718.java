675966c7a13a1460577b1802ca220916505455b3
Su-ho Park <psuhoo122065@gmail.com>
2022-05-16 11:23:07 +0900

    Updated on May 16 2022 11:23

---
+++ Bronze/src/level5/Problem10718.java
@@ -0,0 +1,12 @@
+package level5;
+
+public class Problem10718 {
+
+	// PC Setting 완료 여부 테스트를 위한 문제 풀이
+	// 10718. We love kriii
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
