73f517d0c9455d3a2b8e4347e7c506e4bcaa74a1
Su-ho Park <psuhoo122065@gmail.com>
2022-06-30 14:27:37 +0900

    Updated on Jun 30 2022 14:27

---
+++ Language_Coder_Cpp/Code236/Code236.cpp
@@ -0,0 +1,23 @@
+﻿// Code236.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, num3;
+	cin >> num1 >> num2 >> num3;
+
+	int multi = num1 * num2 * num3;
+	int result = 1;
+
+	while (multi > 0) {
+		int num = multi % 10;
+		if (num != 0) {
+			result *= num;
+		}
+		multi /= 10;
+	}
+
+	cout << result;
+}
---
+++ Language_Coder_Cpp/Code591/Code591.cpp
@@ -0,0 +1,20 @@
+﻿// Code591.cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	int* arr = new int[num + 1];
+
+	arr[1] = 1;
+
+	for (int i = 2; i <= num; i++) {
+		arr[i] = arr[i / 2] + arr[i - 1];
+	}
+
+	cout << arr[num];
+}
